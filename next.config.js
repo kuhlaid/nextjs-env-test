987cf4cd88333155500ac421f7f1c2c96003940a
kuhlaid <41551429+kuhlaid@users.noreply.github.com>
2022-07-18 20:25:28 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  adTenantId: process.env.NON_PUBLIC_TESTEV
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
