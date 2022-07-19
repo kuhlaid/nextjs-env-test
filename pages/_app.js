e4cd8fef02354f8d037c37e71f7167c3c7e317b9
kuhlaid <41551429+kuhlaid@users.noreply.github.com>
2022-07-18 20:06:51 -0400

    Update _app.js
    adding modules

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import { MsalProvider } from '@azure/msal-react';
+import { msalInstance } from 'services/msal';
+import 'bootstrap/dist/css/bootstrap.min.css';  // here we include bootstrap across our app
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MsalProvider instance={msalInstance}>
+      <Component {...pageProps} />
+    </MsalProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
